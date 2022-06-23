b3e39595ac0d128abcbc15f1a7243feef80cacca
Grazvis <grazvydas.untulis@gmail.com>
2022-06-23 18:35:56 +0300

    ex13.19

--- models/index.js
+++ models/index.js
@@ -1,8 +1,12 @@
 const Blog = require('./blog')
 const User = require('./user')
+const ReadingList = require('./readingList')
 
 Blog.belongsTo(User)
 User.hasMany(Blog)
+
+User.belongsToMany(Blog, { through: ReadingList })
+Blog.belongsToMany(User, { through: ReadingList })
 
 module.exports = {
   Blog,
---
+++ models/readingList.js
@@ -0,0 +1,34 @@
+const { DataTypes, Model } = require('sequelize')
+const { sequelize } = require('../util/db')
+
+class ReadingList extends Model {}
+
+ReadingList.init({
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      reference: { model: 'users', key: 'id' },
+    },
+    blogId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      reference: { model: 'blogs', key: 'id' },
+    },
+    read: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    },
+  }, {
+    sequelize,
+    underscored: true,
+    timestamps: true,
+    modelName: 'reading_list'
+})
+
+module.exports = ReadingList
